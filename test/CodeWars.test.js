bf242e52166ff6b92d12cbd7d5ba165ac81800fa
Jack <ajackofsometrades@gmail.com>
2018-10-01 09:06:50 -0700

    set up testing for rot13

--- test/CodeWars.test.js
+++ test/CodeWars.test.js
@@ -3,14 +3,28 @@ const assert = require('assert');
 const CodeWars = require('../lib/CodeWars');
 
 
-describe('CodeWars JS Katas', () => {
+describe('array_diff', () => {
 
-
+    const codewars = new CodeWars();
+    
     it('array_diff', () => {
-        const codewars = new CodeWars();
-
+        
         assert.deepEqual(codewars.array_diff([], [4, 5]), [], 'a was [], b was [4,5]');
         assert.deepEqual(codewars.array_diff([3, 4], [3]), [4], 'a was [3,4], b was [3]');
         assert.deepEqual(codewars.array_diff([1, 8, 2], []), [1, 8, 2], 'a was [1,8,2], b was []');
     });
+    
 }); 
+
+describe('Rot13', () => {
+    const codewars = new CodeWars();
+
+    it('test', () => {
+        assert.equal('grfg', codewars.rot13('test'));
+    });
+
+    it('Test', () => {
+        assert.equal('Grfg', codewars.rot13('Test'));
+    });
+});
+
