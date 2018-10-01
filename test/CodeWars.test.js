7abf1ecaeca54ef4d8c3011f55b1537702028bea
Jack <ajackofsometrades@gmail.com>
2018-10-01 09:59:12 -0700

    changing indents to match codewars format

--- test/CodeWars.test.js
+++ test/CodeWars.test.js
@@ -5,26 +5,26 @@ const CodeWars = require('../lib/CodeWars');
 
 describe('array_diff', () => {
 
-    const codewars = new CodeWars();
+  const codewars = new CodeWars();
     
-    it('array_diff', () => {
+  it('array_diff', () => {
         
-        assert.deepEqual(codewars.array_diff([], [4, 5]), [], 'a was [], b was [4,5]');
-        assert.deepEqual(codewars.array_diff([3, 4], [3]), [4], 'a was [3,4], b was [3]');
-        assert.deepEqual(codewars.array_diff([1, 8, 2], []), [1, 8, 2], 'a was [1,8,2], b was []');
-    });
+    assert.deepEqual(codewars.array_diff([], [4, 5]), [], 'a was [], b was [4,5]');
+    assert.deepEqual(codewars.array_diff([3, 4], [3]), [4], 'a was [3,4], b was [3]');
+    assert.deepEqual(codewars.array_diff([1, 8, 2], []), [1, 8, 2], 'a was [1,8,2], b was []');
+  });
     
 }); 
 
 describe('Rot13', () => {
-    const codewars = new CodeWars();
+  const codewars = new CodeWars();
 
-    it('test', () => {
-        assert.equal(codewars.rot13('test'), 'grfg');
-    });
+  it('test', () => {
+    assert.equal(codewars.rot13('test'), 'grfg');
+  });
 
-    it('Test', () => {
-        assert.equal(codewars.rot13('Test'), 'Grfg');
-    });
+  it('Test', () => {
+    assert.equal(codewars.rot13('Test'), 'Grfg');
+  });
 });
 
